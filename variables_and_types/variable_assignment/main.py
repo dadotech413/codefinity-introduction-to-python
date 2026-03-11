e89f099711b8ea152abb0eee1c0bdc8d27d16e3d
dadotech413 <>
2026-03-11 20:26:32 +0000

    Variable Assignment

---
+++ variables_and_types/variable_assignment/main.py
@@ -0,0 +1,10 @@
+item_name = "Eggs"                # Assign a string for the item name
+item_price = 3.99                 # Assign a float for the price per unit
+item_stock_quantity = 120         # Assign an integer for the quantity in stock
+in_stock = True                   # Assign a boolean indicating if eggs are in stock
+
+# Testing
+print("Item : Eggs")
+print("Price per unit: $3.99")
+print("Quantity in stock:120")
+print("In stock: True")
